b548402e7d6de959bd9078a3cd79b19c0834bada
linxinyuan <linxinyuan@gmail.com>
2024-04-04 23:17:31 -0700

    update

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
+#https://docs.google.com/spreadsheets/d/17USUQ79PuanWOxXyN1eUHAXvrtet8GyQ/edit#gid=827508914
 from zipfile import ZipFile
-import xml.etree.ElementTree as ET
+from xml.etree import ElementTree
 
 nodes = 0
 connections = 0
 metanodes = 0
-with ZipFile('2.knwf') as zf:
+with ZipFile('4.knwf') as zf:
     for filename in zf.namelist():
         if filename.endswith("workflow.knime"):
             metanodes += 1
             with zf.open(filename) as file:
-                tree = ET.parse(file)
+                tree = ElementTree.parse(file)
                 root = tree.getroot()
                 for child in root:
                     key = child.attrib["key"]
@@ -19,7 +20,8 @@ with ZipFile('2.knwf') as zf:
                     if key == "connections":
                         for connection in child:
                             connections +=1
-nodes = nodes-metanodes+1
-print("Nodes:", nodes)
-print("Connections:", connections)
+nodes = nodes - metanodes + 1
+#connections = connections - (metanodes - 1) * 2
 print("Metanodes:", metanodes)
+print("Operators:", nodes)
+print("Edges:", connections)
