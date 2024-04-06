58f1c7dc0387a381903065ee664cdcd450ef5dc9
linxinyuan <linxinyuan@gmail.com>
2024-04-06 04:30:37 -0700

    update

--- parser.py
+++ parser.py
@@ -1,76 +1,83 @@
 #https://docs.google.com/spreadsheets/d/17USUQ79PuanWOxXyN1eUHAXvrtet8GyQ/edit#gid=827508914
 from zipfile import ZipFile
 from xml.etree import ElementTree
-from collections import Counter
 from graphviz import Digraph
 from random import randint
+from collections import Counter
 
 graph = Digraph()
 
 filename = "examples/02.knwf"
-metanodes = 0
-multop = 0
+metanodes = dict()
 nodes = 0
-connections = 0
+edges = []
 
 
 def parse(zf, filename):
-    global multop
     global nodes
-    global connections
+    meta_inputs = []
+    meta_outputs = []
     with zf.open(filename) as file:
         root = ElementTree.parse(file).getroot()
-        outputs = Counter()
-        inputs = Counter()
         for child in root:
             key = child.attrib["key"]
             if key == "nodes":
                 for node in child:
+                    node_id = node.attrib["key"].split("_")[1]
                     for entry in node:
                         if entry.attrib["key"] == "node_settings_file":
                             metafile = entry.attrib["value"]
-                            name = metafile.split("/")[0]
-                            name = name[:name.rfind(" ")]
-                            graph.node(name=node.attrib["key"].split("_")[1], label=name)
-                        if entry.attrib["key"] == "node_is_meta":
-                            if entry.attrib["value"] == "true":
-                                print("meta")
-                                parse(zf, filename[:-14]+metafile)
-                    nodes += 1
+                            value = metafile.split("/")
+                            if value[1] == "workflow.knime":
+                                metanodes[node_id] = parse(zf, filename[:-14] + metafile)
+                            else:
+                                name = value[0]
+                                graph.node(name=node_id, label=name[:name.rfind(" ")])
+                                nodes += 1
             if key == "connections":
                 for connection in child:
-                    connections += 1
                     for entry in connection:
                         if entry.attrib["key"] == "sourceID":
                             source = entry.attrib["value"]
-                            outputs[source] += 1
                         if entry.attrib["key"] == "destID":
                             dest = entry.attrib["value"]
-                            inputs[dest] += 1
-                    graph.edge(source, dest)
-
-        muliopSet = set()
-        for node in inputs:
-            if inputs[node] > 1:
-                muliopSet.add(node)
-        for node in outputs:
-            if inputs[node] > 1:
-                muliopSet.add(node)
-        multop += len(muliopSet)
+                    if source == "-1":
+                        meta_inputs.append(dest)
+                    elif dest == "-1":
+                        meta_outputs.append(source)
+                    else:
+                        edges.append([source, dest])
+    return meta_inputs, meta_outputs
 
 
 with ZipFile(filename) as zf:
     for filename in zf.namelist():
-        if filename.endswith("workflow.knime") and filename.count("/") == 1:
-            print(filename)
+        if filename.count("/") == 1 and filename.endswith("workflow.knime"):
             parse(zf, filename)
 
-graph.attr(rankdir='LR')
-graph.render(filename="graph/"+str(randint(1000000,9999999)), view=True)
+while metanodes.keys() & set(sum(edges, [])):
+    for i, (source, dest) in enumerate(edges):
+        if source in metanodes:
+            for meta_input in metanodes[source][1]:
+                edges[i] = [meta_input, dest]
+        elif dest in metanodes:
+            for meta_output in metanodes[dest][0]:
+                edges[i] = [source, meta_output]
 
-nodes = nodes - metanodes + 1
-#connections = connections - (metanodes - 1) * 2
-print("Metanodes:", metanodes)
+inputs = Counter()
+outputs = Counter()
+
+for source, dest in edges:
+    graph.edge(source, dest)
+    inputs[source] += 1
+    outputs[dest] += 1
+
+mult_op = {node for node in inputs | outputs if inputs[node] > 1 or outputs[node] > 1}
+
+graph.attr(rankdir='LR')
+graph.render(filename="graph/" + str(randint(1000000, 9999999)), view=True)
+
+print("Metanodes:", len(metanodes))
 print("Operators:", nodes)
-print("Edges:", connections)
-print("Operators with multIO:", multop)
+print("Edges:", len(edges))
+print("Operators with multIO:", len(mult_op))
